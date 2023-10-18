33a9b02ad865640b4019533bee4c3bb706612d16
Scott Gibson <scottchgibson@gmail.com>
2023-10-18 13:50:55 -0700

    Styled SessionStats table and card

--- frontend/src/Components/SolvetimesTable.js
+++ frontend/src/Components/SolvetimesTable.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 const SolvetimesTable = () => {
-  return <div>Table</div>;
+  const times = [5.55, 6.74, 8.52];
+
+  return (
+    <table className="table table-sm table-primary table-borderless border border-2 border-dark">
+      <tbody>
+        {times.map((time, id) => (
+          <tr key={id}>
+            <th scope="row" className="text-end">
+              {id}:
+            </th>
+            <td className="text-center">{time}</td>
+            <td>x</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default SolvetimesTable;
--- frontend/src/Components/SummaryCard.js
+++ frontend/src/Components/SummaryCard.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const SummaryCard = () => {
-  return <div>Card</div>;
+  return (
+    <div className="container bg-light border border-2 border-dark">
+      <div className="row text-end">
+        <div className="col">Average: {"00:00.00"}</div>
+        <div className="col">Best: {"00:00.00"}</div>
+      </div>
+      <div className="row text-end">
+        <div className="col">Avg of 5: {"00:00.00"}</div>
+        <div className="col">3 of 5: {"00:00.00"}</div>
+      </div>
+      <div className="row text-end">
+        <div className="col">Avg 10: {"00:00.00"}</div>
+        <div className="col">10 of 12: {"00:00.00"}</div>
+      </div>
+    </div>
+  );
 };
 
 export default SummaryCard;
