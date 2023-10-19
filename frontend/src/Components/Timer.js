560699682a529b38d75083a31db0d11e3d5c3e51
Scott Gibson <scottchgibson@gmail.com>
2023-10-19 14:39:27 -0700

    Implement main Timer interactivity
    The timer now starts on Space keyup, and stops on Space keydown.
    The time displayed on the page takes the computed time and formats
    it to a nice syntax.

--- frontend/src/Components/Timer.js
+++ frontend/src/Components/Timer.js
@@ -1,28 +1,73 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { getScramble } from "../Utils/CubeUtils";
 
 const Timer = () => {
-  const [scramble, setScramble] = useState("getting scramble...");
-  const [start, setStart] = useState(0);
+  /* State vars */
+
+  const [scramble, setScramble] = useState(getScramble);
+  const [timerText, setTimerText] = useState("00:00.000");
   const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
 
-  // get scramble on page load
+  let intervalRef = useRef(0);
+
+  /* Timer text formatting */
+
   useEffect(() => {
-    setScramble(getScramble());
-    setStart(window.performance.now());
-  }, []);
-
-  // create interval for timer
-  useEffect(() => {
-    const dur = 37;
-    const interval = setInterval(() => {
-      setTime(window.performance.now() - start);
-    }, dur);
-
-    return () => clearInterval(interval);
+    setTimerText(formatTime());
   }, [time]);
 
+  const formatTime = () => {
+    let timeString = "MM:SS.sss";
+    let m = Math.floor(time / 1000 / 60).toString();
+    let s = Math.floor((time / 1000) % 60).toString();
+    let ms = Math.floor(time % 1000).toString();
+    m = m.padStart(2, "0");
+    s = s.padStart(2, "0");
+    ms = ms.padEnd(3, "0");
+    timeString = timeString.replace("MM", m);
+    timeString = timeString.replace("SS", s);
+    timeString = timeString.replace("sss", ms);
+
+    return timeString;
+  };
+
+  /* Spacebar interactivity */
+
+  const handleSpaceUp = () => {
+    if (!running) {
+      const fps = 30; // how often the timer updates per second
+      const start = performance.now();
+      intervalRef.current = setInterval(() => {
+        setTime(performance.now() - start);
+      }, 1000 / fps);
+    }
+    setRunning((running) => !running);
+  };
+  const handleSpaceDown = () => {
+    if (intervalRef.current !== 0) {
+      clearInterval(intervalRef.current);
+    }
+  };
+
+  /* Document keyboard event handling */
+
+  let once = false; // flag to keep onkeydown to only trigger once
+  document.onkeyup = (e) => {
+    if (e.key === " ") {
+      handleSpaceUp();
+      once = false;
+    }
+  };
+  document.onkeydown = (e) => {
+    if (!once && e.key === " ") {
+      handleSpaceDown();
+      once = true;
+    }
+  };
+
   /* PAGE CONTENT */
+
   return (
     <div className="h-100 d-flex flex-column justify-content-between">
       <div className="container">
@@ -30,14 +75,11 @@ const Timer = () => {
         <div className="bg-secondary text-white text-center font-monospace">
           {scramble}
         </div>
-        <div className="bg-secondary text-white text-center font-monospace">
-          real time: {(time / 1000).toFixed(3)}
-        </div>
-        <h1 className="text-center fst-italic">{"00:00.000"}</h1>
+        <h1 className="text-center fst-italic font-monospace">{timerText}</h1>
       </div>
 
       <div className="container my-3">
-        <p className="text-center fst-italic fw-bold">
+        <p className="text-center fst-italic fw-bold font-monospace">
           Press space to start timer
         </p>
       </div>
